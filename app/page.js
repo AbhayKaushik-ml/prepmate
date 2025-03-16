c75bfe25421c4083c034bc650fabbd6f6f452aea
Abhay Kaushik <kaushikabhay777@gmail.com>
2025-03-16 14:11:38 +0530

    Fix deployment issues: add autoprefixer and fix import paths

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import Image from "next/image";
 
 export default function LandingPage() {
